19089826987bb9c045d063ac2c96125604f43908
Valentina Kononenko <valentina@kononenko.net>
2018-09-21 21:59:42 -0400

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,54 @@
 package com.company;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s1, s2;
+        String status = "";
+        int length, l1, l2, i, j;
+        int found = 0, notFound = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter the first word: ");
+        s1 = scan.nextLine();
+        System.out.print("Please enter the second word: ");
+        s2 = scan.nextLine();
+
+        l1 = s1.length();
+        l2 = s2.length();
+
+        if(l1 == l2)
+        {
+            length = l1;
+            for(i = 0; i < length; i++)
+            {
+                found = 0;
+                for(j = 0; j < length; j++)
+                {
+                    if (s1.charAt(i) == s2.charAt(j))
+                    {
+                        found = 1;
+                        break;
+                    }
+                }
+                if(found == 0)
+                {
+                    notFound = 1;
+                    break;
+                }
+            }
+            if(notFound == 1)
+            {
+                status = "not ";
+            }
+        }
+        else
+        {
+            status = "not ";
+        }
+        System.out.println(s1 + " and " + s2 + " are " + status + "anagrams of each other.");
     }
 }
